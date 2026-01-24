e1afdadf0bafcf86bbe415b0b8b9b86049739234
garv rawal <kumargarv05@gmail.com>
2026-01-24 21:44:53 +0530

    add user account management routes for password change, current user retrieval, and updates to avatar and cover image

--- scr/routes/user.route.js
+++ scr/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { logoutUser, loginUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { logoutUser, loginUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateUserDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getUserHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,6 +24,17 @@ router.route('/login').post(loginUser)
 //secure routes
 router.route('/logout').post(verifyJWT, logoutUser)
 router.route('/refresh-token').post(refreshAccessToken)
+router.route('/change-password').post(verifyJWT, changeCurrentPassword)
+router.route('/current-user').get(verifyJWT, getCurrentUser)
+
+// patch use for the update only the perticuler fields
+router.route('/update-account').patch(verifyJWT, updateUserDetails)
+router.route('/user-avatar').post().patch(verifyJWT, upload.single("avatar"), updateUserAvatar) 
+router.route('/cover-image').patch(verifyJWT, upload.single("coverImage"),updateUserCoverImage)
+
+// get used for the fetching the data
+router.route('/c/:username').get(verifyJWT, getUserChannelProfile)
+router.route('/history').get(verifyJWT, getUserHistory)
     
 
 export default router 
